ec3894d8ef3f2d784ba3409f30eff37cac914a07
Iann Dang <iann1978@163.com>
2021-05-20 10:56:56 +0800

    - (show_coco_mask) rewrite it with opencv

--- demos/show_coco_mask.py
+++ demos/show_coco_mask.py
@@ -4,38 +4,53 @@ from pycocotools.coco import COCO
 from skimage import io
 from matplotlib import pyplot as plt
 import matplotlib.patches as patches
+import cv2
+import numpy as np
+
 
 # dataDir=r'G:\datasets\coco\2017'
 dataDir = r'C:\work\githome\PyTorch\geojson2coco\data\outcoco'
 dataDir = r'E:\work\githome\PyTorch\mmdetection\mydemos\TestDataFromGEOJSON_Simple\data'
-# dataDir = r'G:\datasets\coco\2017'
+dataDir = r'G:\datasets\coco\2017'
 dataType = 'val2017'
 annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
 imgDir = os.path.join(dataDir, dataType)
 
 coco = COCO(annFile)
 
-catIds = coco.getCatIds(catNms=['horse']) # catIds=1 表示人这一类
-#catIds = coco.getCatIds(catNms=['person']) # catIds=1 表示人这一类
-imgIds = coco.getImgIds(catIds=catIds ) # 图片id，许多值
-imageCount = len(imgIds)
+catIds = coco.getCatIds('horse')
+imgIds = coco.getImgIds(catIds=catIds)
+annIds = coco.getAnnIds(imgIds=imgIds)
 
-for i in range(len(imgIds)):
-    fig, ax = plt.subplots()
+imgs = coco.loadImgs(imgIds)
 
-    img = coco.loadImgs(imgIds[i])[0]
+for img in imgs:
     imgFile = os.path.join(imgDir, img['file_name'])
-    I = io.imread(imgFile)
-    ax.axis('off')
-    ax.imshow(I) #绘制图像，显示交给plt.show()处理
-    annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
-    anns = coco.loadAnns(annIds)
-    # bbox = anns[0]['bbox']
+    image = cv2.imread(imgFile)
 
-    coco.showAnns(anns)
+    annIds = coco.getAnnIds(imgIds=[img['id']], catIds=catIds)
+    anns = coco.loadAnns(annIds)
+
     for ann in anns:
-        bbox = ann['bbox']
-        rect = patches.Rectangle((bbox[0], bbox[1]), bbox[2], bbox[3],
-                                 linewidth=1, edgecolor='r', facecolor='none')
-        ax.add_patch(rect)
-    plt.show() #显示图像
+        # Draw bbox
+        x, y, w, h = ann['bbox']
+        image = cv2.rectangle(image, (int(x), int(y)), (int(x + w), int(y + h)), (0, 255, 255), 2)
+
+        # Draw segmentation
+        segs = ann["segmentation"]
+        segs = [np.array(seg, np.int32).reshape((1, -1, 2))
+                for seg in segs]
+        for seg in segs: cv2.drawContours(image, seg, -1, (0, 255, 0), 2)
+
+        mask = coco.annToMask(ann)
+        mask1 = np.transpose(mask)
+        mask2 = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+        image = image + mask2*128
+        mask3 = mask2*255
+        image = cv2.addWeighted(image, 1, mask3, 0.3, 0.0)
+        shape = mask1.shape
+        shape1 = image.shape
+
+    cv2.imshow('demo', image)
+    cv2.waitKey(5000)
+
